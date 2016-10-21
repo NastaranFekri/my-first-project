c645698a3ce18c0e001c51095004f0384dad7d27
Nastaran Fekri <nastaran@delodi.net>
2016-10-21 20:48:25 +0200

    Initial commit

---
+++ src/com/github/nastarafekri/Main.java
@@ -0,0 +1,10 @@
+package com.github.nastarafekri;
+
+/**
+ * Created by Nastaran on 21/10/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hallo world");
+    }
+}
